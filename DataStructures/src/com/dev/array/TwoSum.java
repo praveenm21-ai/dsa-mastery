04a756deaa5bf16509aff6f469b57a711fb07afd
Praveen <praveensurya518@gmail.com>
2025-08-02 23:01:57 +0530

    two sum,three sum,Rearranging String

---
+++ DataStructures/src/com/dev/array/TwoSum.java
@@ -0,0 +1,18 @@
+package com.dev.array;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {0, -1, 2, -3, 1};
+		int target=-2;
+		twoSum(arr,target);
+
+	}
+
+	private static int twoSum(int arr[], int target) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ DataStructures/src/com/dev/string/RearrangingString.java
@@ -0,0 +1,16 @@
+package com.dev.string;
+
+public class RearrangingString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="AC2BEW3";
+		reArrangeString(s);
+	}
+
+	private static void reArrangeString(String s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
