dc2aa8533a4315f0720de5a753f63208dca08f03
Praveen <praveensurya518@gmail.com>
2025-09-21 19:48:10 +0530

    palindrome string

---
+++ DataStructures/src/com/dev/string/PalindromeString.java
@@ -0,0 +1,17 @@
+package com.dev.string;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+       String s= "hello world";
+       String ans=reverseString(s);
+       System.out.println(ans);
+    }
+
+    private static String reverseString(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(int i=s.length()-1;i>=0;i--){
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+}
